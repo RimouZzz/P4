a7217b0e5759e8d4db12d2c3158a1d74269f482a
kristian1402 <72245684+kristian1402@users.noreply.github.com>
2022-04-04 17:13:09 +0200

    vinkler fikset og cleanet lidt op

--- Sound2.py
+++ Sound2.py
@@ -1,6 +1,7 @@
 import random
 from playsound import playsound #Version 1.2.2
 
+
 def AudioDirection():
     i = 0
     sounds = {
@@ -10,27 +11,30 @@ def AudioDirection():
         'mood': ["Neutral", "Neutral"]
     }
 
-    while(i < 15):
-        #First number = which sound 2nd = direction of sound between two speakers
-        j = random.randint(0,1)
+    while i < 15:
+        j = random.randint(0, 1)
 
-        chosenSpeaker = random.randint(0,7) #Pick random speaker
-        speaker2 = chosenSpeaker +1
-        if chosenSpeaker == 7:
-            speaker2 = 0
+        speakerAngleVals = [0, 45, 90, 135, 180, 225, 270, 315]
+        currentSpeakerAngle = random.choice(speakerAngleVals)
+        speakerNr1 = speakerAngleVals.index(currentSpeakerAngle)
+        speakerNr2 = speakerNr1+1
+        if speakerNr1 == 7:
+            speakerNr2 = 0
+        currentDegree = currentSpeakerAngle + sounds['orientation'][j]
 
-        print(f"playing sound {sounds['name'][j]} on speakers {chosenSpeaker} and {speaker2} at angle {sounds['orientation'][j]}")
-        playsound(sounds['file'][j])
+        print(f"playing {sounds['mood'][j]} sound {sounds['name'][j]} "
+              f"on speakers {speakerNr1} and {speakerNr2} at angle {currentDegree}°")
+        playsound(sounds['file'][j], block=False)
 
         while True:
-            answer = sounds['orientation'][j]
             guess = int(input("Type in the angle: "))
-            if guess == answer:
+            if guess == currentDegree:
                 i += 1
                 break
             else:
                 print("try again")
 
         print(" ")
+
 
 AudioDirection()
